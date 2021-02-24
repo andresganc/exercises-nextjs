2800457028bb99a725c1c985a5fed0be978f98b4
Andres Giraldo <andresganc@gmail.com>
2021-02-24 11:11:12 -0500

    New Pruebas curso

--- README.md
+++ README.md
@@ -18,3 +18,17 @@ Curso Next.js NC
         $ cd my-app
 
         $ npm run dev
+
+
+### Images
+
+    - Se debe instalar la libreria next-images para trabajar con imagenes SVG
+
+        $ npm i next-images
+
+        - Despues de instalar se debe crear un archivo en la raiz llamado next.config.js y escribir las siguiente lineas de codigo
+
+        const withImages = require('next-images')
+        module.exports = withImages({})
+
+
--- course-nc/pages/index.js
+++ course-nc/pages/index.js
@@ -1,20 +1,33 @@
 import Head from 'next/head'
-import Link from 'next/link'
-import { Fragment } from 'react'
 
-// Views
-import Navigation from './navigation/navigation'
-
-
-import styles from '../styles/Home.module.css'
+// NC Components
+import Prueba01 from '../components/prueba/prueba-01'
 
 export default function Home() {
   return (
-    <Fragment>
+    <div>
 
-      <Navigation />
+      <Head>
+        <title> Next </title>
+      </Head>
+
+      <main>
+        
+        <h2> ESTO ES NEXT </h2>
+
+        <Prueba01 />
+      </main>
+
+      <section>
+
+      </section>
+
+
+      <footer>
+        <h2> ESTE ES EL FOOTER DE NEXT </h2>
+      </footer>
       
-    </Fragment>
+    </div>
     
   )
 }
