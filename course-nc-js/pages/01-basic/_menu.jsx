82cec09e16df0b20c2b152ced93709785afd83a2
Andres Giraldo <andresganc@gmail.com>
2022-08-16 14:44:28 -0500

    Mod: Menu

--- course-nc-js/pages/01-basic/_menu.jsx
+++ course-nc-js/pages/01-basic/_menu.jsx
@@ -1,39 +1,100 @@
 
 import Link from 'next/link'
 import Icon from '@mdi/react'
+import Styled from 'styled-components'
 
 // NC Components Base
 import Div from '@components/div'
-import Nav from '@components/nav'
+import H6 from '@components/h6'
 import A from '@components/a'
-import Button from '@components/button'
+import P from '@components/p'
 
 // Icons 
-import { mdiHome, mdiLink } from '@mdi/js'
+import { mdiHome, mdiLinkLock, mdiLink, mdiLinkPlus } from '@mdi/js'
+
+
+const Card = Styled(Div) `
+    display: grid;
+    justify-content: center;
+    align-items: center;
+    width: 200px;
+    height: 150px;
+    border-radius: 8px;
+    background-color: #F5F5F5;
+    padding: 10px;
+    cursor: pointer;
+    transition: .3s;
+    margin: 5px;
+    text-align: center;
+
+    &:hover {
+        background-color: #B3E5FC;
+        box-shadow: 0 0 10px #9E9E9E;
+    }
+
+`
 
 const Menu = () => {
     return (
         <>
-            <Nav>
-                <Button cursorPointer marginX1 paddingX15 paddingY8 borderRadius1>
-                    <Div>
-                        <Icon path={mdiHome} title="Home" size={1.5} color='#616161' />
-                    </Div>
-                    <Div>
-                        <Link href='/'><A textGray800 >Home</A></Link>
-                    </Div>     
-                </Button>
+            <Div displayFlex>
+                <Card>
+                    <Link href='/'>
+                        <Div displayGrid jcCenter aiCenter>
+                            <Div>
+                                <Icon path={mdiHome} title="Home" size={1.5} color='#616161' />
+                            </Div>
+                            <Div>
+                                <H6 textGray800>Home</H6>
+                                <P fontSize8 textGray500 marginT2>Routing static using link into next </P>
+                            </Div>  
+                        </Div>     
+                    </Link>
+                </Card>
 
-                <Button cursorPointer marginX2 paddingX15 paddingY8 borderRadius1>
-                    <Div>
-                        <Icon path={mdiLink} title="Home" size={1.5} color='#616161' />
-                    </Div>
-                    <Div>
-                        <Link href=''><A textGray800>Routing static</A></Link>
-                    </Div>     
-                </Button>
+                <Card>
+                    <Link href='/'>
+                        <Div displayGrid jcCenter aiCenter>
+                            <Div>
+                                <Icon path={mdiLinkLock} title="Routing static" size={1.5} color='#616161' />
+                            </Div>
+                            <Div>
+                                <H6 textGray800>Routing static</H6>
+                                <P fontSize8 textGray500 marginT2>Routing static using link into next </P>
+                            </Div>
+                        </Div>     
+                    </Link>
+                </Card>
+
+                <Card>
+                    <Link href='/'>
+                        <Div displayGrid jcCenter aiCenter>
+                            <Div>
+                                <Icon path={mdiLink} title="Routing dynamic" size={1.5} color='#616161' />
+                            </Div>
+                            <Div>
+                                <H6 textGray800>Routing dynamic</H6>
+                                <P fontSize8 textGray500 marginT2>Routing static using link into next </P>
+                            </Div> 
+                        </Div>     
+                    </Link>
+                </Card>
+
+                <Card>
+                    <Link href='/'>
+                        <Div displayGrid jcCenter aiCenter>
+                            <Div>
+                                <Icon path={mdiLinkPlus} title="Routing dynamic" size={1.5} color='#616161' />
+                            </Div>
+                            <Div>
+                                <H6 textGray800>Redirect</H6>
+                                <P fontSize8 textGray500 marginT2>Redirect into next </P>
+                            </Div> 
+                        </Div>     
+                    </Link>
+                </Card>
                 
-            </Nav>
+            </Div>
         </>
     )
 }
