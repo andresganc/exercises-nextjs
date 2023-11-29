e2c309a1b60bdf16dbe770b7587b6445269c34e5
Andres Giraldo <andresganc@gmail.com>
2023-11-29 10:17:58 -0500

    Clear Repository

--- course-nc/components/assets/data/company/company-size.js
+++
@@ -1,9 +0,0 @@
-
-export const companySize = [
-
-    { name: "Microempresa", size: "1 - 9 Empleados" },
-    { name: "Pequeña Empresa", size: "10 - 49 Empleados" },
-    { name: "Mediana Empresa", size: "50 - 249 Empleados" },
-    { name: "Gran Empresa", size: "250 o mas Empleados" },
-
-]
